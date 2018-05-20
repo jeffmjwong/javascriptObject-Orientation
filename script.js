55464232e3255729ba8b8d51e212b6d9d6886dd9
Jeff Wong <jeffmjwong@gmail.com>
2018-05-20 14:12:33 +1000

    Object literal

---
+++ script.js
@@ -0,0 +1,14 @@
+const fruits = ['apple', 'pear', 'strawberry']
+console.log(fruits.length)
+
+const denis = {
+  animal: 'dog',
+  age: 20,
+  color: 'red',
+  bark: function() {
+    console.log('Woof!')
+  }
+}
+
+const x = 'animal'
+console.log(denis[x])
